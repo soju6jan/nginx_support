e973ced3f6d0a66bc0bb99d8c509c696616a69f1
soju6jan <cybersol@naver.com>
2020-10-04 23:08:44 +0900

    update

---
+++ install/Organizr.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /app/data/nginx/www
+git clone https://github.com/causefx/Organizr
+chmod 777 -R /app/data/nginx/www/Organizr
--- install/myComix.sh
+++ install/myComix.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /app/data/nginx/www
 git clone https://github.com/imueRoid/myComix
-chmod 777 -R /app/data/nginx/www
+chmod 777 -R /app/data/nginx/www/myComix
