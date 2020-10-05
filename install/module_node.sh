a0fd33767a33e4ea5d0df4a2d115007d22f27315
soju6jan <cybersol@naver.com>
2020-10-05 18:51:31 +0900

    Update module_node.sh

--- install/module_node.sh
+++ install/module_node.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-if [ "$#" -lt 1 ] || ["$1" == "install" ] ; then
+if [ "$#" -lt 1 ] || [ "$1" == "install" ] ; then
   mkdir -p /app/data/nginx/nodejs
   apk update
   apk upgrade
   apk add nodejs npm
+  echo "npm --version"
+  echo `npm --version`
+  echo "node --version"
+  echo `node --version`
 else
   apk del nodejs npm
 fi
