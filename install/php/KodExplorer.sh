468c788ca20c9180854da8d076d40404ccf76f41
soju6jan <cybersol@naver.com>
2020-10-07 19:08:43 +0900

    Update KodExplorer.sh

--- install/php/KodExplorer.sh
+++ install/php/KodExplorer.sh
@@ -2,6 +2,7 @@
 cd /app/data/nginx/www
 git clone https://github.com/kalcaddle/KodExplorer --depth 1
 chmod 777 -R /app/data/nginx/www/KodExplorer
+mkdir -p /app/data/nginx/www/KodExplorer/data/Group/public/home/share
 ln -s /app/data /app/data/nginx/www/KodExplorer/data/Group/public/home/share/sjva_data
 cat <<EOF >/app/data/nginx/www/KodExplorer/config/define.php
 <?php define ('DATA_PATH', '/app/data/nginx/www/KodExplorer/data/');
